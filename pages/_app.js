d4dc387a575e549c9b1ebf3425edb170f9e66ed2
priyanshu2304 <priyanshumodi1998@gmail.com>
2024-12-27 11:24:38 +0530

    code cleanup

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-import "@/styles/globals.css";
-import Header from "@/components/Header";
+import '@/styles/globals.css';
+import '@/styles/header.css';
+import Header from '@/components/Header';
 export default function App({ Component, pageProps }) {
   return (
     <div>
